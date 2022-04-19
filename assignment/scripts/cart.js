f91012c9234167ba3ae1482375162a72b8185886
Jonathan Palalay <ring5pun@hotmail.com>
2022-04-19 17:57:02 -0500

    created function to list items in Basket

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,25 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+
+function addItem (item){
+    basket.push(item);
+    return true;
+}
+
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log('Adding bananas (expect true)', addItem('bananas'));
+console.log('Adding oranges (expect true)', addItem('oranges'));
+console.log(`Basket is now ${basket}`);
+
+function listItems(){
+    for (let i = 0; i < basket.length; i++) {
+        console.log(basket[i]);        
+    }
+}
+
+listItems(); //test to see if it console logs each item in the basket
+
